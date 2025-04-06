c606c133eb738ac98d29afe473ffc92aa5d3be7b
snakeztc <tianchez@hzlh.com>
2025-04-06 18:01:41 +0800

    add action history

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ if __name__ == "__main__":
     model = VLM("llm.yaml")
     floor_id = 'FloorPlan10'
     model_id = "Qwen/Qwen2.5-VL-32B-Instruct"
-    task_prompt = "You are a robot that is navigating a house. You are given a list of last N image of your first person view from the oldest to the most recent. You current task is to find {OBJ} in the room and get in front of it, meaning the object should be in the center of the image and occupy most of the image. Given this task, you need to choose your next action from the following options: {ACTIONS}. Output only the action you choose, no other text. When you are in front of the object, you should stop and output 'Done'."
-    target = "a white fridge on the left"
+    task_prompt = "You are a robot that is navigating in a house. You are given a list of last N image of your first person views from the oldest to the most recent. Your previous action history is {ACTION_HISTORY} from the oldest to the most recent. You current task is to find {OBJ} and get very close to it. When you cannot immediately find the object, you should rotate around the room to find it. Given this task, you need to choose your next action from the following options: {ACTIONS}. Output only the action you choose, no other text. Only output'Done' if the target object is in the center of the image and occupy most of the image."
+    target = "a white fridge on the left side of the room"
     buffer_len = 5
-    memory = []
+    view_memory = []
+    act_memory = []
 
     controller = Controller(
         agentMode="default",
@@ -36,10 +37,11 @@ if __name__ == "__main__":
         fieldOfView=90
     )
 
-    actions = ['MoveAhead', 'RotateRight', 'RotateLeft', 'MoveLeft', 'MoveRight']
+    actions = ['MoveAhead', 'RotateRight', 'RotateLeft', 'MoveLeft', 'MoveRight', "Done"]
     event = controller.step(action="Done") # ge tthe initial image
     view = event.cv2img
-    memory.append(view)
+    view_memory.append(view)
+    act_memory.append("Init")
 
     if not os.path.exists('views'):
         os.makedirs('views')
@@ -49,11 +51,12 @@ if __name__ == "__main__":
         os.makedirs('views')
 
     for i in range(50):
-        # keep the last buffer_len views in memory
-        memory = memory[-buffer_len:]
+        # keep the last buffer_len views in view_memory
+        view_memory = view_memory[-buffer_len:]
 
-        a = model.run(memory, model_id, task_prompt.format(OBJ=target, ACTIONS=actions))
-        print(a, len(memory))
+        prompt = task_prompt.format(OBJ=target, ACTIONS=actions, ACTION_HISTORY=act_memory)
+        a = model.run(view_memory, model_id, prompt)
+        print(a, len(view_memory), len(act_memory), prompt)
 
         if 'Rotate' in a:
             event = controller.step(action=a, degrees=30)
@@ -66,5 +69,6 @@ if __name__ == "__main__":
         view = event.cv2img
         cv2.imwrite(f'views/view_{i}_{a}.png', view)
 
-        memory.append(view)
+        view_memory.append(view)
+        act_memory.append(a)
 
