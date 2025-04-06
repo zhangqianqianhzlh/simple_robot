87b372a7d50e1f6bfab09510b44cb23eb0452e6b
snakeztc <tianchez@hzlh.com>
2025-04-06 16:30:02 +0800

    init commit

---
+++ test_map.py
@@ -0,0 +1,54 @@
+from ai2thor.controller import Controller
+import numpy as np
+import cv2
+import os
+import shutil
+
+if __name__ == "__main__":
+
+    controller = Controller(
+        agentMode="default",
+        visibilityDistance=1.5,
+        scene="FloorPlan10",
+
+        # step sizes
+        gridSize=0.25,
+        snapToGrid=True,
+        rotateStepDegrees=90,
+
+        # image modalities
+        renderDepthImage=False,
+        renderInstanceSegmentation=False,
+
+        # camera properties
+        width=300,
+        height=300,
+        fieldOfView=90
+    )
+
+    actions = ['MoveAhead', 'RotateRight', 'RotateLeft', 'MoveLeft', 'MoveRight']
+    
+    if not os.path.exists('views'):
+        os.makedirs('views')
+    else:
+        # remove the view folder and make a new one
+        shutil.rmtree('views')
+        os.makedirs('views')
+
+    for i in range(100):
+        a = np.random.choice(actions)
+        if 'Rotate' in a:
+            event = controller.step(action=a, degrees=15)
+        else:
+            event = controller.step(action=a)
+
+        view = event.cv2img
+        # save the view to a folder, if the folder doesn't exist, create it
+
+        cv2.imwrite(f'views/view_{i}_{a}.png', view)
+
+
+    
+
+        
+        
