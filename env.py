3149606cfae131d9bf470e4055236a12fd4f82d3
snakeztc <tianchez@hzlh.com>
2025-04-07 11:35:19 +0800

    add env class

---
+++ env.py
@@ -0,0 +1,44 @@
+import yaml
+from models import VLM
+from ai2thor.controller import Controller
+import numpy as np
+import cv2
+import os
+import shutil
+
+
+class ThorEnv(object):
+    def __init__(self, floor_id) -> None:
+       self.controller = Controller(
+            agentMode="default",
+            visibilityDistance=1.5,
+            scene=floor_id,
+
+            # step sizes
+            gridSize=0.25,
+            snapToGrid=True,
+            rotateStepDegrees=90,
+
+            # image modalities
+            renderDepthImage=False,
+            renderInstanceSegmentation=False,
+
+            # camera properties
+            width=300,
+            height=300,
+            fieldOfView=90
+    )
+       
+    def get_last_event(self):
+        return self.controller.last_event
+       
+    def step(self, action):
+        if 'Rotate' in action:
+            event = self.controller.step(action=action, degrees=30)
+        elif 'Done' in action:
+            event = self.get_last_event()
+        else:
+            event = self.controller.step(action=action)
+        
+        return event
+
--- main.py
+++ main.py
@@ -1,45 +1,24 @@
-import yaml
 from models import VLM
-from ai2thor.controller import Controller
-import numpy as np
 import cv2
 import os
 import shutil
+from env import ThorEnv
 
 
 if __name__ == "__main__":
     model = VLM("llm.yaml")
     floor_id = 'FloorPlan10'
     model_id = "Qwen/Qwen2.5-VL-32B-Instruct"
-    task_prompt = "You are a robot that is navigating in a house. You are given a list of last N image of your first person views from the oldest to the most recent. Your previous action history is {ACTION_HISTORY} from the oldest to the most recent. You current task is to find {OBJ} and get very close to it. When you cannot immediately find the object, you should rotate around the room to find it. Given this task, you need to choose your next action from the following options: {ACTIONS}. Output only the action you choose, no other text. Only output'Done' if the target object is in the center of the image and occupy most of the image."
-    target = "a white fridge on the left side of the room"
+    task_prompt = "You are a robot that is navigating in a house. You are given a list of last N image of your first person views from the oldest to the most recent. Your previous action history is {ACTION_HISTORY} from the oldest to the most recent. You current task is to follow the instructions: '{TARGET}' and get very close to it. When you cannot immediately find the object, you should rotate around the room to find it. Given this task, you need to choose your next action from the following options: {ACTIONS}. Output only the action you choose, no other text. Only output'Done' if the target object is in the center of the image and occupy most of the image."
+    target = "turn left and move forward and then find the white fridge on your right hand side"
     buffer_len = 5
     view_memory = []
     act_memory = []
 
-    controller = Controller(
-        agentMode="default",
-        visibilityDistance=1.5,
-        scene=floor_id,
-
-        # step sizes
-        gridSize=0.25,
-        snapToGrid=True,
-        rotateStepDegrees=90,
-
-        # image modalities
-        renderDepthImage=False,
-        renderInstanceSegmentation=False,
-
-        # camera properties
-        width=300,
-        height=300,
-        fieldOfView=90
-    )
+    env = ThorEnv(floor_id)
 
     actions = ['MoveAhead', 'RotateRight', 'RotateLeft', 'MoveLeft', 'MoveRight', "Done"]
-    event = controller.step(action="Done") # ge tthe initial image
-    view = event.cv2img
+    view = env.get_last_event().cv2img # ge tthe initial image
     view_memory.append(view)
     act_memory.append("Init")
 
@@ -54,21 +33,20 @@ if __name__ == "__main__":
         # keep the last buffer_len views in view_memory
         view_memory = view_memory[-buffer_len:]
 
-        prompt = task_prompt.format(OBJ=target, ACTIONS=actions, ACTION_HISTORY=act_memory)
+        prompt = task_prompt.format(TARGET=target, ACTIONS=actions, ACTION_HISTORY=act_memory)
         a = model.run(view_memory, model_id, prompt)
         print(a, len(view_memory), len(act_memory), prompt)
-
-        if 'Rotate' in a:
-            event = controller.step(action=a, degrees=30)
-        elif 'Done' in a:
-            print("I am done")
+        if a == 'Done':
             break
-        else:
-            event = controller.step(action=a)
 
+        event = env.step(a)
+        
         view = event.cv2img
         cv2.imwrite(f'views/view_{i}_{a}.png', view)
 
         view_memory.append(view)
         act_memory.append(a)
+
+
+    print("FINISHED")
 
